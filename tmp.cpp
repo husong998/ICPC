bc0fbffaee97571b8adb60cde8507caee9e9124e
husong998 <34832485+husong998@users.noreply.github.com>
2019-06-28 21:03:13 +0800

    initial commit

--- tmp.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main() {
-  char tmp[4][20] = {"asfa", "sdafd", "weqt", "oiup"};
-  sort(tmp, tmp + 4, [](char* a, char* b) -> bool { return strcmp(a, b) < 0; });
-  for (int i = 0; i < 4; ++i) cout << tmp[i] << endl;
-  return 0;
-}
--- tmp.py
+++
@@ -1,7 +0,0 @@
-from sys import *
-for i in range(10):
-  stdout.write('"' + str(i) + 'T", ')
-for i in range(10):
-  stdout.write('"' + str(i) + 'S", ')
-for i in range(10):
-  stdout.write('"' + str(i) + 'W", ')
